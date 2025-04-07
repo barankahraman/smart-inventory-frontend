650fb9479ac961c7ee663fd673c4cf939e1ed60f
hakandrs <hakandurusan@gmail.com>
2025-04-07 16:23:47 +0300

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,5 +2,5 @@ import { Navigate } from 'react-router-dom';
 
 export default function PrivateRoute({ children }) {
   const isAuthenticated = !!localStorage.getItem("user"); // You can check token/session instead
-  return isAuthenticated ? children : <Navigate to="/" />;
+  return isAuthenticated ? children : <Navigate to="/" replace />;
 }
